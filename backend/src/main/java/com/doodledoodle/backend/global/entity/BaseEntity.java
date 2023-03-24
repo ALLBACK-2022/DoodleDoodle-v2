5a7cc2b76f9e7928eef2c06b782aa7af5d55e5fe
Jung Yunho <a01049048063@gmail.com>
2023-03-24 21:12:04 +0900

    feat: BaseEntity추가

---
+++ backend/src/main/java/com/doodledoodle/backend/global/entity/BaseEntity.java
@@ -0,0 +1,24 @@
+package com.doodledoodle.backend.global.entity;
+
+import java.time.LocalDateTime;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@MappedSuperclass
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+
+  @CreatedDate
+  private LocalDateTime createdAt;
+
+  @LastModifiedDate
+  private LocalDateTime updatedAt;
+}
