837789caf07172c81cd5c0a6b1c925cb3401bec5
JeongHunHui <deca3248@gmail.com>
2023-04-03 02:26:50 +0900

    fix: axios util 수정

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -6,15 +6,21 @@ const axiosInstance = axios.create({
   baseURL: BASEURL,
 });
 
-axiosInstance.interceptors.request.use(
-  config => {
-    return config;
-  },
-  error => {
-    console.log('axiosInstance request Error');
-    return Promise.reject(error);
-  },
-);
+// axiosInstance.interceptors.request.use(
+//   config => {
+//     return config;
+//   },
+//   error => {
+//     console.log('axiosInstance request Error');
+//     return Promise.reject(error);
+//   },
+// );
+
+axiosInstance.interceptors.request.use(config => {
+  // eslint-disable-next-line no-param-reassign
+  config.url = `${BASEURL}${config.url}`; // Replace with your API URL
+  return config;
+});
 
 axiosInstance.interceptors.response.use(
   response => response,
