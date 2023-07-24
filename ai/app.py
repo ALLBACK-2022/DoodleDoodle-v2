c1cd80b52db60fdfefa785245388644986120b95
Jung Yunho <a01049048063@gmail.com>
2023-07-24 14:13:53 +0900

    feat: ai 관련 로직 변경
    파일을 넘기지 않고 파일명 관련 데이터를 넘겨서 이미지를 s3에서 직접 받음 + 임시 tmp폴더를 생성해 이미지를 받음

--- ai/app.py
+++ ai/app.py
@@ -3,44 +3,49 @@ from skimage.transform import resize
 from faust import app, result_topic
 import matplotlib.pyplot as plt
 import numpy as np
+import tempfile
+from connection import s3_connection, s3_download_image
 
 @app.agent(result_topic)
 async def search(result):
     async for res in result:
         
+        s3 = s3_connection()
         random_word = str(res['randomWord'])
-        image_file = res['file']
-        draw_id = str(res['drawId'])
-        
-        with open("./ai-model/class_names.txt", "r", encoding="utf8") as file:
-            class_names = []
-            for line in file:
-                class_names.append(line.rstrip('\n'))
+        draw_id = str(res['drawId'])  
+        file_name = f"{draw_id}.png"
+        with tempfile.TemporaryDirectory() as temp_dir:
+            local_file_path = os.path.join(temp_dir, file_name)
+            file = s3_download_image(s3, file_name, local_file_path)
+            
+            with open("./ai-model/class_names.txt", "r", encoding="utf8") as file:
+                class_names = []
+                for line in file:
+                    class_names.append(line.rstrip('\n'))
+                    
+            model = keras.models.load_model('./model/keras.h5')
 
-        model = keras.models.load_model('./model/keras.h5')
+            image = plt.imread(image_file)
+            image = image[:, :, 0]
+            image = resize(image, (28, 28))
 
-        image = plt.imread(image_file)
-        image = image[:, :, 0]
-        image = resize(image, (28, 28))
+            for x in range(0, 28):
+                for y in range(0, 28):
+                    num = image[x][y] - 1
+                    if num < 0:
+                        num *= -1
+                    image[x][y] = num
 
-        for x in range(0, 28):
-            for y in range(0, 28):
-                num = image[x][y] - 1
-                if num < 0:
-                    num *= -1
-                image[x][y] = num
+            pred = model.predict(np.expand_dims(image, axis=0))[0]
+            ind = (-pred).argsort()[:]
 
-        pred = model.predict(np.expand_dims(image, axis=0))[0]
-        ind = (-pred).argsort()[:]
-        
-        response, result = {}, {}
-        
-        for x in range(0, len(ind)):
-            if(class_names[ind[x]] == random_word):
-                result[class_names[ind[x]]] = round(pred[ind[x]]*100, 2)
-            if x < 5:
-                response[class_names[ind[x]]] = round(pred[ind[x]]*100, 2)
+            response, result = {}, {}
 
+            for x in range(0, len(ind)):
+                if(class_names[ind[x]] == random_word):
+                    result[class_names[ind[x]]] = round(pred[ind[x]]*100, 2)
+                if x < 5:
+                    response[class_names[ind[x]]] = round(pred[ind[x]]*100, 2)
         response['result'] = result
         response['drawId'] = draw_id
 
